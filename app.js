49b24932c57afba1ed71cf785d4851c853eee515
elbek <elbeksaparbayev05@gmail.com>
2025-11-08 22:22:20 +0500

    1-qism

---
+++ app.js
@@ -0,0 +1,6 @@
+const night = document.getElementById("high-button")
+const header = document.getElementById('header')
+night.addEventListener('click', ()=>{
+//    header.style.backgroundColor == black
+console.log("salom")
+})
